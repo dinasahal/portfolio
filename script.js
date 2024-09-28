b27c75346a7f4c61e1c9a254bd62237e0bcf5d11
dinasahal <dina12sahal@gmail.com>
2024-09-28 13:19:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission for demonstration
+    alert('Thank you for your message!');
+});
